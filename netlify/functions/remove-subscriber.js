b079b89eefcad0ed12844adf304f9b73c677c719
Andrew Bloyce <bloyce1990@gmail.com>
2022-12-19 21:48:10 +1000

    404 and unsubscribe page

---
+++ netlify/functions/remove-subscriber.js
@@ -0,0 +1,25 @@
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import { createClient } from '@supabase/supabase-js'
+
+dotenv.config()
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(process.env.DATABASE, process.env.DATABASE_KEY)
+
+export const handler = async (event, context) => {
+	const { email } = JSON.parse(event.body)
+
+	const { data, error } = await supabase
+		.from('subscribers')
+		.delete()
+		.eq('email', email)
+
+	return {
+		statusCode: 200,
+		body: JSON.stringify({
+			status: 200,
+			message: data,
+			error: error
+		}),
+	}
+}
