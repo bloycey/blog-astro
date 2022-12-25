f66d467d34cf8b5e8d5f81cc85939f29bc44321b
Andrew Bloyce <bloyce1990@gmail.com>
2022-12-25 17:13:54 +1000

    About me page

--- netlify/functions/add-comment.js
+++ netlify/functions/add-comment.js
@@ -1,5 +1,8 @@
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 import { createClient } from '@supabase/supabase-js'
+const rateLimit = require("lambda-rate-limiter")({
+	interval: 60 * 1000 // Our rate-limit interval, one minute
+}).check;
 
 dotenv.config()
 
@@ -8,6 +11,20 @@ const supabase = createClient(process.env.DATABASE, process.env.DATABASE_KEY)
 
 export const handler = async (event, context) => {
 	const { name, comment, blog_id, last_name } = JSON.parse(event.body)
+	try {
+		// 10 stands for the maximum amount of requests allowed during the defined interval
+		// rateLimit now returns a promise, let's await for it! (◕‿◕✿)
+		await rateLimit(2, event.headers["client-ip"]);
+	} catch (error) {
+		console.log("slow down! Too many requests!")
+		return {
+			statusCode: 429,
+			body: JSON.stringify({
+				status: 429,
+				message: 'Too Many Requests',
+			}),
+		}; // Still returning a basic 429, but we could do anything~
+	}
 	if (last_name) {
 		return {
 			statusCode: 200,
