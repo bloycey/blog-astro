07bab905582a3f7d85ed5ee2e1b375aa67471b0f
Andrew Bloyce <bloyce1990@gmail.com>
2023-01-14 14:43:23 +1000

    SEO-ing

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
+  site: "https://bloycey.blog/",
   markdown: {
     shikiConfig: {
       theme: 'dracula',
